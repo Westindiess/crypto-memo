3c53638bc1b2900c3539b483665ce56205a69c8d
Westindiess <gregoryalexiss@gmail.com>
2021-06-07 11:15:25 +0200

    useMemo

--- src/components/List.js
+++ src/components/List.js
@@ -1,36 +1,40 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { useCurrencies } from "../hooks/useCurrencies"
 import Currency from "./Currency"
 
 const List = () => {
-  const { error, loading, currencies } = useCurrencies()
-  const [active, setActive] = useState(null)
-  const displayedCurrencies = currencies.slice(0, 500)
+	const { error, loading, currencies } = useCurrencies()
+	const [active, setActive] = useState(null)
+	const displayedCurrencies = currencies.slice(0, 500)
 
-  return (
-    <div className="container p-4 py-5 px-lg-5">
-      {loading ? (
-        <p>loading... it can take a while...</p>
-      ) : (
-        <>
-          <p>
-            {displayedCurrencies.length} first results / {currencies.length}
-          </p>
-          {displayedCurrencies.map((el) => {
-            return (
-              <Currency
-                key={el.id}
-                currency={el}
-                isActive={el.id === active}
-                setActive={setActive}
-              />
-            )
-          })}
-        </>
-      )}
-      {error && <p>{error}</p>}
-    </div>
-  )
+	const hideDetails = useCallback(() => setActive(null), [])
+	const showDetails = useCallback((id) => setActive(id), [])
+
+	return (
+		<div className="container p-4 py-5 px-lg-5">
+			{loading ? (
+				<p>loading... it can take a while...</p>
+			) : (
+				<>
+					<p>
+						{displayedCurrencies.length} first results / {currencies.length}
+					</p>
+					{displayedCurrencies.map((el) => {
+						return (
+							<Currency
+								key={el.id}
+								currency={el}
+								isActive={el.id === active}
+								hideDetails={hideDetails}
+								showDetails={showDetails}
+							/>
+						)
+					})}
+				</>
+			)}
+			{error && <p>{error}</p>}
+		</div>
+	)
 }
 
 export default List
