87d00db96e00d54a3ac8d23affbc9e36920d6eab
Westindiess <gregoryalexiss@gmail.com>
2021-06-07 11:47:50 +0200

    useMemo

--- src/components/List.js
+++ src/components/List.js
@@ -1,54 +1,60 @@
-import { useState, useCallback } from "react"
+import { matchSorter } from "match-sorter"
+import { useState, useCallback, useMemo } from "react"
 import { useCurrencies } from "../hooks/useCurrencies"
 import Currency from "./Currency"
 
 const List = () => {
-  const { error, loading, currencies } = useCurrencies()
-  const [filter, setFilter] = useState("")
+	const { error, loading, currencies } = useCurrencies()
+	const [filter, setFilter] = useState("")
 
-  const [active, setActive] = useState(null)
-  const displayedCurrencies = currencies.slice(0, 500)
+	const [active, setActive] = useState(null)
 
-  const hideDetails = useCallback(() => setActive(null), [])
-  const showDetails = useCallback((id) => setActive(id), [])
+	const displayedCurrencies = useMemo(() => {
+		const filteredCurrencies = filter
+			? matchSorter(currencies, filter, { keys: ["name", "symbol"] })
+			: currencies
+		return filteredCurrencies.slice(0, 500)
+	}, [filter, currencies])
+	const hideDetails = useCallback(() => setActive(null), [])
+	const showDetails = useCallback((id) => setActive(id), [])
 
-  return (
-    <div className="container p-4 py-5 px-lg-5">
-      {loading ? (
-        <p>loading... it can take a while...</p>
-      ) : (
-        <>
-          <div className="mb-3">
-            <label htmlFor="filter" className="form-label">
-              Filter currencies
-            </label>
-            <input
-              id="filter"
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-              className="form-control"
-            />
-          </div>
-          <p>
-            {displayedCurrencies.length} first results / {currencies.length}
-          </p>
-          {displayedCurrencies.map((el) => {
-            return (
-              <Currency
-                key={el.id}
-                currency={el}
-                isActive={el.id === active}
-                showDetails={showDetails}
-                hideDetails={hideDetails}
-                setActive={setActive}
-              />
-            )
-          })}
-        </>
-      )}
-      {error && <p>{error}</p>}
-    </div>
-  )
+	return (
+		<div className="container p-4 py-5 px-lg-5">
+			{loading ? (
+				<p>loading... it can take a while...</p>
+			) : (
+				<>
+					<div className="mb-3">
+						<label htmlFor="filter" className="form-label">
+							Filter currencies
+						</label>
+						<input
+							id="filter"
+							value={filter}
+							onChange={(e) => setFilter(e.target.value)}
+							className="form-control"
+						/>
+					</div>
+					<p>
+						{displayedCurrencies.length} first results / {currencies.length}
+					</p>
+					{displayedCurrencies.map((el) => {
+						return (
+							<Currency
+								key={el.id}
+								currency={el}
+								isActive={el.id === active}
+								showDetails={showDetails}
+								hideDetails={hideDetails}
+								setActive={setActive}
+							/>
+						)
+					})}
+				</>
+			)}
+			{error && <p>{error}</p>}
+		</div>
+	)
 }
 
 export default List
