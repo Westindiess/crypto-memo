0b4a129231de540ddca8e01431c13f5ce87cac8f
Westindiess <gregoryalexiss@gmail.com>
2021-09-01 10:33:05 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { useState } from "react"
 import List from "./components/List"
+import { CurrenciesContextProvider } from "./context/CurrenciesContext"
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false)
-  return (
-    <div className={` min-vh-100 ${darkMode ? "bg-dark text-light" : ""}`}>
-      <button
-        type="button"
-        className="btn btn-warning fixed-top"
-        onClick={() => setDarkMode(!darkMode)}
-      >
-        change
-      </button>
-      <header className="App-header text-center bg-dark text-white p-5">
-        <h1>
-          Crypto Currencies with{" "}
-          <a className="text-reset" href="https://nomics.com/docs/">
-            nomics API
-          </a>
-        </h1>
-      </header>
-      <List />
-    </div>
-  )
+	const [darkMode, setDarkMode] = useState(false)
+	return (
+		<div className={` min-vh-100 ${darkMode ? "bg-dark text-light" : ""}`}>
+			<button
+				type="button"
+				className="btn btn-warning position-absolute"
+				onClick={() => setDarkMode(!darkMode)}
+			>
+				change
+			</button>
+			<CurrenciesContextProvider>
+				<header className="App-header text-center bg-dark text-white p-5">
+					<h1>
+						Crypto Currencies with{" "}
+						<a className="text-reset" href="https://nomics.com/docs/">
+							nomics API
+						</a>
+					</h1>
+				</header>
+				<List />
+			</CurrenciesContextProvider>
+		</div>
+	)
 }
 
 export default App
--- src/hooks/useCurrencies.js
+++ src/hooks/useCurrencies.js
@@ -1,39 +1,39 @@
 import { useEffect, useReducer } from "react"
 import { reducer } from "../reducers"
 export const useCurrencies = () => {
-  const [{ loading, error, currencies }, dispatch] = useReducer(reducer, {
-    loading: false,
-    error: "",
-    currencies: []
-  })
-  useEffect(() => {
-    let isCancelled = false
-    const API_KEY = process.env.REACT_APP_API_KEY
-    dispatch({ type: "FETCH_INIT" })
-    fetch(
-      `https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&interval=1d,30d&convert=EUR`
-    )
-      .then((response) => {
-        console.log(response)
-        if (!response.ok) {
-          throw new Error("Désolée, nous avons rencontré un problème...")
-        }
-        return response.json()
-      })
-      .then((data) => {
-        if (!isCancelled) {
-          dispatch({ type: "FETCH_SUCCESS", payload: data })
-        }
-      })
-      .catch((error) => {
-        console.log(error.message)
-        if (!isCancelled) {
-          dispatch({ type: "FETCH_FAILURE", payload: error.message })
-        }
-      })
-    return () => {
-      isCancelled = true
-    }
-  }, [])
-  return { loading, error, currencies }
+	const [{ loading, error, currencies }, dispatch] = useReducer(reducer, {
+		loading: false,
+		error: "",
+		currencies: [],
+	})
+	useEffect(() => {
+		let isCancelled = false
+		const API_KEY = "65152846747017a31a43299341bf46e2f33e485d"
+		dispatch({ type: "FETCH_INIT" })
+		fetch(
+			`https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&interval=1d,30d&convert=EUR`
+		)
+			.then((response) => {
+				console.log(response)
+				if (!response.ok) {
+					throw new Error("Désolée, nous avons rencontré un problème...")
+				}
+				return response.json()
+			})
+			.then((data) => {
+				if (!isCancelled) {
+					dispatch({ type: "FETCH_SUCCESS", payload: data })
+				}
+			})
+			.catch((error) => {
+				console.log(error.message)
+				if (!isCancelled) {
+					dispatch({ type: "FETCH_FAILURE", payload: error.message })
+				}
+			})
+		return () => {
+			isCancelled = true
+		}
+	}, [])
+	return { loading, error, currencies }
 }
